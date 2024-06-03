081bed79e073eebaac039a6e3c03e379e3b60ab4
Udayanga Sameera Dilshan <146975240+usamd@users.noreply.github.com>
2024-06-04 00:21:03 +0530

    update migrations and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -48,4 +49,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function messages(): HasMany
+    {
+        return $this->HasMany(Message::class);
+    }
+
 }
