2f3ade18baa575d6e7f939b7fd06cfb8d270272e
ashanisamarakoon <ashikaushalya06@gmail.com>
2024-06-05 16:55:20 +0530

    Customized signin page for employee and client

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Pbd/v1Vc3lIISBWkXNtHKCrz8o+PQy4bO3kq6KJXQpM6wiK8WA/Cu8rE9bhEQtH8D" crossorigin="anonymous">
+
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
