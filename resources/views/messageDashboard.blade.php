9becd6f2ea62f0240b0cdc7d542d5037b26287b2
Udayanga Sameera Dilshan <146975240+usamd@users.noreply.github.com>
2024-06-14 12:42:23 +0530

    Some_Changes_on_Interface
    Connecting Landing_page
    Changes on chat UI

--- resources/views/messageDashboard.blade.php
+++ resources/views/messageDashboard.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My Messenger App</title>
-  <link rel="stylesheet" href="interface.css"> 
+  <link rel="stylesheet" href="messageInterface.css"> 
   <style>
     
   </style>
