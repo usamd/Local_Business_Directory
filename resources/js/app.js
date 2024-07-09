67cd46fe6123f2e2e240c4227aa59e2fb842aaf3
ashanisamarakoon <ashikaushalya06@gmail.com>
2024-07-09 14:33:23 +0530

    Test commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,3 @@
 import './bootstrap';
-import { createApp } from 'vue';
-import Chat from './chat.vue';
-// import Alpine from 'alpinejs'; // Uncomment if you are using Alpine.js
+import '@fortawesome/fontawesome-free/js/all.js';
 
-const app = createApp(Chat);
-app.mount('#chatapp');
-
-// Uncomment if you are using Alpine.js
-// window.Alpine = Alpine;
-// Alpine.start();
